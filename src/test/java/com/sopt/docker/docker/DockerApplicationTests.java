cf672e606d9332ea0dca0e91e6150f7a0657a415
DongHoon Lee <dhl1924@naver.com>
2024-06-22 14:33:39 +0900

    test(DockerApplicationTests): build용 코드 추가

--- src/test/java/com/sopt/docker/docker/DockerApplicationTests.java
+++ src/test/java/com/sopt/docker/docker/DockerApplicationTests.java
@@ -3,7 +3,7 @@ package com.sopt.docker.docker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DockerApplicationTests.class)
 class DockerApplicationTests {
 
 	@Test
